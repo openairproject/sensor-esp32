bec5209637af19a733a4e8ba41bab24161b0e999
kglowacki <kglowacki@gmail.com>
2017-10-05 23:15:03 +0200

    jenkins fix

--- bin/erase.sh
+++ bin/erase.sh
@@ -1,1 +1,1 @@
-python $IDF_PATH/components/esptool_py/esptool/esptool.py -p /dev/tty.SLAB_USBtoUART -b 115200 erase_flash
+python $IDF_PATH/components/esptool_py/esptool/esptool.py -p /opt/oap/dev/ttyOAP -b 115200 erase_flash
