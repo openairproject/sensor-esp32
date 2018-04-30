a499c8f7d6945f7bc5d38deedd3599935b4503d6
BayCom GmbH <software@baycom.de>
2018-04-30 11:17:36 +0200

    change defaults, add pcnt code to gpio, fix race condition in GPIO-ISR, version 1.4.2

--- components/oap-hw-gpio/include/hw_gpio.h
+++ components/oap-hw-gpio/include/hw_gpio.h
@@ -38,12 +38,14 @@ typedef struct {
 	uint8_t output_pin;
 	uint8_t input_pin;
 	QueueHandle_t gpio_evt_queue;
+	QueueHandle_t pcnt_evt_queue;
 	int64_t GPIlastLow;
 	int64_t GPIlastHigh;
 	int64_t GPOlastOut;
 	int64_t GPICounter;
 	int32_t GPOtriggerLength;
 	int8_t GPOlastval;
+	int64_t lastPublish;
 } hw_gpio_config_t;
 
 extern hw_gpio_config_t hw_gpio_cfg[];
